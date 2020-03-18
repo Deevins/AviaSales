c52534654469a28fa815f492b375b60c6f19baf7
deevins <daker255@bk.ru>
2020-03-18 19:35:20 +0300

    fixed a few bugs and beautified code

--- script.js
+++ script.js
@@ -1,76 +1,61 @@
 const formSearch = document.querySelector('.form-search'),
-      inputCitiesFrom = document.querySelector('.input__cities-from'),
-      dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
-      inputCitiesTo = document.querySelector('.input__cities-to'),
-      dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
-      inputDateDepart = document.querySelector('.input__date-depart');
+    inputCitiesFrom = document.querySelector('.input__cities-from'),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to'),
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    inputDateDepart = document.querySelector('.input__date-depart');
 
 
 
-      const cities = ['Moscow', 'St.-Petersburg','Minsk','Chelyabinsk',
-                      'Volgograd', 'Samara', 'Ekaterinburg', 'Odessa',
-                      'Uhan', 'Shimkenty','Down-Novgorod','Kaliningrad', 'Rostov-na-Dony'];
+const cities = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керчь', 'Симферополь', 'Волгоград', 'Самара', 'Днепропетровск', 'Екатеринбург', 'Одесса', 'Ухань', 'Нижний Новгород', 'Калининград', 'Вроцлав', 'Ростов-на-Дону', 'Киев', 'Владивосток', 'Нью-Йорк', 'Лондон', 'Тегеран', 'Канны', 'Оттава'];
 
 
 
-const ShowCities = (input,list) => {
+const showCities = (input, list) => {
     list.textContent = '';
-    if(input.value !== '') {
+
+    if (input.value !== '') {
         const citiesFilter = cities.filter((item) => {
             const fixedItem = item.toLowerCase();
-            return fixedItem.includes(inputCitiesFrom.value.toLowerCase());
+            return fixedItem.includes(input.value.toLowerCase());
         });
-    
+
         citiesFilter.forEach((item) => {
             const li = document.createElement('li');
             li.classList.add('dropdown__city');
             li.textContent = item;
-            dropdownCitiesFrom.append(li);
+            list.append(li);
         });
     }
 };
 
-
+//listeners
 inputCitiesFrom.addEventListener('input', () => {
-    ShowCities(inputCitiesFrom, dropdownCitiesFrom);
+    showCities(inputCitiesFrom, dropdownCitiesFrom);
 });
+
+inputCitiesTo.addEventListener('input', () => {
+    showCities(inputCitiesTo, dropdownCitiesTo);
+});
+
+const citiesPush =(input,list) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        input.value = target.textContent;
+        list.textContent = '';
+
+    }
+
+};
+
+
 
 
 dropdownCitiesFrom.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesFrom.value = target.textContent;
-        dropdownCitiesFrom.textContent = '';
-        
-    }
-});
+    citiesPush(inputCitiesFrom,dropdownCitiesFrom);
 
-
- const showCitiesTo = (input,list) => {
-     list.textContent = '';
-     if(input.value !== '') {
-        const citiesFilter = cities.filter((item) => {
-            const fixedItem = item.toLowerCase();
-            return fixedItem.includes(inputCitiesTo.value.toLowerCase());
-        });
-        citiesFilter.forEach((item) => {
-            const li = document.createElement('li');
-            li.classList.add('dropdown__cities-to');
-            li.textContent = item;
-            dropdownCitiesTo.append(li);
-        });
-     }
- };
-
- inputCitiesTo.addEventListener('input', () => {
-    showCitiesTo(inputCitiesTo, dropdownCitiesTo);
 });
 
 dropdownCitiesTo.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesTo.value = target.textContent;
-        dropdownCitiesTo.textContent = '';
-        
-    }
+    citiesPush(inputCitiesTo,dropdownCitiesTo);
 });
